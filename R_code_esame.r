df19fad859759c9677f38770d478e2937c1d1767
Marcolozzaa <62596264+Marcolozzaa@users.noreply.github.com>
2020-06-09 19:07:29 +0200

    Update R_code_esame.r

--- R_code_esame.r
+++ R_code_esame.r
@@ -11,7 +11,37 @@ library(rgdal)
 
 
 
-#### FUNZIONE DI UNIRE LE COASTLINES
+
+
+#### CARICO LE IMMAGINI                  zlim, è 0,5 giusto? si puo fare di meglio? IO FAREI 4
+
+
+setwd("~/Desktop/ESAME COPERNICUS/HarvAreaYield_4Crops_95-00-05_Geotiff/Soybean")
+soy_1995 <- raster("Soybean_1995_Yield.tif")
+soy_2000 <- raster("Soybean_2000_Yield.tif")
+soy_2005 <- raster("Soybean_2005_Yield.tif")
+# cerco su internet tutto l'arrange dei colori di R e cerco i colori giusti 
+cl <- colorRampPalette(c('aliceblue','darkgoldenrod1','darkgoldenrod4'))(100)
+
+# plotto le immagini
+plot(soy_1995,col=cl)
+plot(soy_1995,col=cl,zlim=c(0,4)) # cambio la zlim per avere le aree meglio rappresentate
+
+par(mfrow=c(2,2))
+plot(soy_1995,col=cl,zlim=c(0,4))
+plot(soy_2000,col=cl,zlim=c(0,4))
+plot(soy_2005,col=cl,zlim=c(0,4))  # le mappe pero non hanno le coastlines, le aggiungo
+
+dev.off()
+
+
+
+
+
+
+
+
+#### FUNZIONE PER UNIRE LE COASTLINES
 
 
 
@@ -25,13 +55,41 @@ coastline <- readOGR("ne_10m_coastline.shp")
 
 setwd("~/Desktop/ESAME COPERNICUS/HarvAreaYield_4Crops_95-00-05_Geotiff/Soybean")
 
-soy_1995 <- raster("Soybean_1995_Yield.tif")
-
 cl <- colorRampPalette(c('aliceblue','darkgoldenrod1','darkgoldenrod4'))(100)
+plot(soy_1995,col=cl,zlim=c(0,4))
 
-plot(soy_1995,col=cl,zlim=c(0,5))
+plot(coastline,add=T)   # aggiungo le coastlines all'immagine raster e lo posso fare perche sono entrambe WGS84
 
-plot(coastline,add=T)   # aggiungo le coastlines all'immagine raster
+# cambio pero la larghezza delle linee delle coaste per farle piu sottili con "lwd"
+
+plot(coastline,lwd=0.3)
+
+plot(soy_1995,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
+
+
+# le coastlines e le immagini tif pero hanno leggermente degli extent diversi per cui le allineo
+
+coastlines  # extension : -180, 180, -85.22194, 83.6341  (xmin, xmax, ymin, ymax)
+soy_1995    # extension : -180, 180, -90, 90  (xmin, xmax, ymin, ymax)
+
+# creo una nuova extension da applicare a tutte le immagini raster
+
+extension <- c(-180, 180, -85.22194, 83.6341)
+soy_1995 <- crop(soy_1995, extension) # mantengo i nomi originali ma li cambio l'estensione
+soy_2000 <- crop(soy_2000, extension)
+soy_2005 <- crop(soy_2005, extension)
+
+par(mfrow=c(2,2))
+plot(soy_1995,col=cl,zlim=c(0,4))  # riplotto tutto insieme aggiungendo le coastlines
+plot(coastline,lwd=0.3,add=T)
+plot(soy_2000,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
+plot(soy_2005,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
+
+
+
 
 
 
@@ -40,19 +98,14 @@ plot(coastline,add=T)   # aggiungo le coastlines all'immagine raster
 #### FUNZIONE DIFFERENZA
 
 
-soy_1995 <- raster("Soybean_1995_Yield.tif")
-soy_2005 <- raster("Soybean_2005_Yield.tif")
-
-cl <- colorRampPalette(c('aliceblue','darkgoldenrod1','darkgoldenrod4'))(100)
-
-
 diff <- soy_2005-soy_1995
 
-plot(diff,col=cl,zlim=c(0,5))
-plot(coastline,add=T)
+cl <- colorRampPalette(c('aliceblue','darkgoldenrod1','darkgoldenrod4'))(100)
+plot(diff,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
 
 
-#### FUNZIONE DI CARICAMENTO TUTTI I DATI ASSIEME
+
 
 
 
@@ -68,22 +121,23 @@ plot(coastline,add=T)
 extension <- c(-100, -20, -60, 20)   # zona SUD AMERICA
 
 soy_1995_SA <- crop(soy_1995, extension)
+soy_2000_SA <- crop(soy_2000, extension)
 soy_2005_SA <- crop(soy_2005, extension)
 
-plot(soy_1995_SA,col=cl,zlim=c(0,5))
-plot(coastline,add=T)   # vedo se funziona
+plot(soy_1995_SA,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)   # vedo se funziona
 
 
 par(mfrow=c(1,2))                       # le metto una di finaco all'altra
-plot(soy_1995_SA,col=cl,zlim=c(0,5))
-plot(coastline,add=T)
-plot(soy_2005_SA,col=cl,zlim=c(0,5))
-plot(coastline,add=T)
+plot(soy_1995_SA,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
+plot(soy_2005_SA,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
 
 
 
 
-###### PROVO CON LE PATCHES
+###### DIFFERENZA CON I CROP DEL SUD AMERICA
 
 
 # posso fare la differenza dei due crop in SUD AMERICA
@@ -91,8 +145,8 @@ plot(coastline,add=T)
 diff_soy_SA <- soy_2005_SA - soy_1995_SA
 cl <- colorRampPalette(c('aliceblue','darkgoldenrod1','darkgoldenrod4'))(100)
 
-plot(diff_soy_SA,col=cl,zlim=c(0,5))
-plot(coastline,add=T)
+plot(diff_soy_SA,col=cl,zlim=c(0,4))
+plot(coastline,lwd=0.3,add=T)
 
 
 
@@ -101,17 +155,25 @@ plot(coastline,add=T)
 
 
 
+#### FUNZIONE DI CARICAMENTO TUTTI I DATI ASSIEME
 
 
-
-
-
-
+###### PROVO CON LE PATCHES
 
 
 #### BOXPLOT 
 
 boxplot(soy_1995) # cosi và, ma sarebbe da cambiare i valori 
+
+#### altre funzioni nel pacchetto raster??
+
+
+
+
+
+
+
+
 
 
 
